5684cfb1ac446801b03c91789ad1b5b3e3284a3a
Daniel Borkmann <daniel@netsniff-ng.org>
2009-12-27 10:16:36 +0000

    More friendly packet output.
    git-svn-id: http://netsniff-ng.googlecode.com/svn/trunk@74 21e0ff64-9a0b-11de-825e-994487f65616

--- src/lib/print.c
+++ src/lib/print.c
@@ -70,97 +70,125 @@
 #include <netsniff-ng/types.h>
 #include <netsniff-ng/print.h>
 
-static void inline dump_hex(const char *desc, ring_buff_bytes_t * buff, int len)
+static void inline dump_hex(ring_buff_bytes_t * buff, int len)
 {
-	dbg("%s", desc);
-
 	while (len--) {
 		dbg("%.2x ", *buff);
 		buff++;
 	}
-
-	dbg("\n");
 }
 
 /*
- * Just plain dumb formatting, for -ccc
+ * dump_ethhdr_all - Just plain dumb formatting, for -ccc
+ * @eth:            ethernet header
  */
 static void inline dump_ethhdr_all(struct ethhdr *eth)
 {
-	dbg("  Ethernet Hdr\n");
+	dbg(" [ ");
 
-	dump_hex("    Dst MAC:   ", eth->h_dest, ETH_ALEN);
-	dump_hex("    Src MAC:   ", eth->h_source, ETH_ALEN);
-	dump_hex("    Proto:     ", (ring_buff_bytes_t *) & eth->h_proto, 2);
+	dbg("MAC (%.2x:%.2x:%.2x:%.2x:%.2x:%.2x => %.2x:%.2x:%.2x:%.2x:%.2x:%.2x), ",
+	    /* Source MAC */
+	    ((uint8_t *) eth->h_source)[6], ((uint8_t *) eth->h_source)[7],
+	    ((uint8_t *) eth->h_source)[8], ((uint8_t *) eth->h_source)[9],
+	    ((uint8_t *) eth->h_source)[10], ((uint8_t *) eth->h_source)[11],
+	    /* Destination MAC */
+	    ((uint8_t *) eth->h_dest)[0], ((uint8_t *) eth->h_dest)[1],
+	    ((uint8_t *) eth->h_dest)[2], ((uint8_t *) eth->h_dest)[3],
+	    ((uint8_t *) eth->h_dest)[4], ((uint8_t *) eth->h_dest)[5]);
+
+	dbg("Proto (0x%.2x%.2x)",
+	    ((uint8_t *) & eth->h_proto)[0], ((uint8_t *) & eth->h_proto)[1]);
+
+	dbg(" ] ");
 }
 
 /*
- * Just plain dumb formatting, for -ccc
+ * dump_iphdr_all - Just plain dumb formatting, for -ccc
+ * @ip:            ip header
  */
 static void inline dump_iphdr_all(struct iphdr *ip)
 {
-	dbg("  IP Hdr\n");
+	dbg(" [ ");
 
-	dbg("    Dst Addr:  %u.%u.%u.%u\n", ((uint8_t *) & ip->daddr)[0],
-	    ((uint8_t *) & ip->daddr)[1], ((uint8_t *) & ip->daddr)[2],
-	    ((uint8_t *) & ip->daddr)[3]);
-	dbg("    Src Addr:  %u.%u.%u.%u\n", ((uint8_t *) & ip->saddr)[0],
-	    ((uint8_t *) & ip->saddr)[1], ((uint8_t *) & ip->saddr)[2],
-	    ((uint8_t *) & ip->saddr)[3]);
-	dbg("    Proto:     %u\n", ip->protocol);
-	dbg("    TTL:       %u\n", ip->ttl);
-	dbg("    TOS:       %u\n", ntohs(ip->tos));
-	dbg("    Version:   %u\n", ntohs(ip->version));
-	dbg("    IHL:       %u\n", ntohs(ip->ihl));
-	dbg("    Total len: %u\n", ntohs(ip->tot_len));
-	dbg("    ID:        %u\n", ntohs(ip->id));
-	dbg("    Frag off:  %u\n", ip->frag_off);
-	dbg("    Checksum:  %x\n", ntohs(ip->check));
+	dbg("Addr (%u.%u.%u.%u => %u.%u.%u.%u), ",
+	    ((uint8_t *) & ip->saddr)[0], ((uint8_t *) & ip->saddr)[1],
+	    ((uint8_t *) & ip->saddr)[2], ((uint8_t *) & ip->saddr)[3],
+	    ((uint8_t *) & ip->daddr)[0], ((uint8_t *) & ip->daddr)[1],
+	    ((uint8_t *) & ip->daddr)[2], ((uint8_t *) & ip->daddr)[3]);
+
+	dbg("Proto (%u), ", ip->protocol);
+	dbg("TTL (%u), ", ip->ttl);
+	dbg("TOS (%u), ", ntohs(ip->tos));
+	dbg("Ver (%u), ", ntohs(ip->version));
+	dbg("IHL (%u), ", ntohs(ip->ihl));
+	dbg("Tlen (%u), ", ntohs(ip->tot_len));
+	dbg("ID (%u), ", ntohs(ip->id));
+	dbg("Frag off (%u), ", ip->frag_off);
+	dbg("Chsum (0x%x)", ntohs(ip->check));
+
+	dbg(" ] ");
 }
 
 /*
- * Just plain dumb formatting, for -ccc
+ * dump_tcphdr_all - Just plain dumb formatting, for -ccc
+ * @tcp:            tcp header
  */
 static void inline dump_tcphdr_all(struct tcphdr *tcp)
 {
-	dbg("  TCP Hdr\n");
+	dbg(" [ ");
 
-	dbg("    Src Port:  %u\n", ntohs(tcp->source));
-	dbg("    Dst Port:  %u\n", ntohs(tcp->dest));
-	dbg("    SN:        %x\n", ntohs(tcp->seq));
-	dbg("    AckN:      %x\n", ntohs(tcp->ack_seq));
-	dbg("    Data off:  %d\n", ntohs(tcp->doff));
-	dbg("    Res 1:     %d\n", ntohs(tcp->res1));
-	dbg("    Flags:\n");
+	dbg("Port (%u => %u), ", ntohs(tcp->source), ntohs(tcp->dest));
+	dbg("SN (0x%x), ", ntohs(tcp->seq));
+	dbg("AN (0x%x), ", ntohs(tcp->ack_seq));
+	dbg("Data off (%d), ", ntohs(tcp->doff));
+	dbg("Res 1 (%d), ", ntohs(tcp->res1));
+
+	dbg("Flags (");
 
 	if (tcp->urg == 1) {
-		dbg("        URG\n");
+		dbg("URG ");
 	}
 	if (tcp->ack == 1) {
-		dbg("        ACK\n");
+		dbg("ACK ");
 	}
 	if (tcp->psh == 1) {
-		dbg("        PSH\n");
+		dbg("PSH ");
 	}
 	if (tcp->rst == 1) {
-		dbg("        RST\n");
+		dbg("RST ");
 	}
 	if (tcp->syn == 1) {
-		dbg("        SYN\n");
+		dbg("SYN ");
 	}
 	if (tcp->fin == 1) {
-		dbg("        FIN\n");
+		dbg("FIN ");
 	}
 	if (tcp->ece == 1) {
-		dbg("        ECE\n");
+		dbg("ECE ");
 	}
 	if (tcp->cwr == 1) {
-		dbg("        CWR\n");
+		dbg("CWR ");
 	}
 
-	dbg("    Window:    %d\n", ntohs(tcp->window));
-	dbg("    Hdr sum:   %d\n", ntohs(tcp->check));
-	dbg("    Urg ptr:   %u\n", ntohs(tcp->urg_ptr));
+	dbg("), ");
+
+	dbg("Window (%d), ", ntohs(tcp->window));
+	dbg("Hdrsum (0x%x), ", ntohs(tcp->check));
+	dbg("Urg ptr (%u)", ntohs(tcp->urg_ptr));
+
+	dbg(" ] ");
+}
+
+/*
+ * dump_payload_all - Just plain dumb formatting, for -ccc
+ * @rbb:             payload bytes
+ * @len:             len
+ */
+static void inline dump_payload_all(ring_buff_bytes_t * rbb, int len)
+{
+	dbg(" [ Payload (");
+	dump_hex(rbb, len);
+	dbg(") ] ");
 }
 
 /**
@@ -170,20 +198,34 @@ static void inline dump_tcphdr_all(struct tcphdr *tcp)
  */
 void print_packet_buffer_mode_1(ring_buff_bytes_t * rbb, int len)
 {
-	dbg("%d Byte\n", len);
+	size_t off_n, off_o;
+
+	dbg("%d Byte: \n", len);
 
 	dump_ethhdr_all((struct ethhdr *)rbb);
+	dbg("\n");
+	off_n = sizeof(struct ethhdr);
 
 	/* Check for IP */
 	if (ntohs(((struct ethhdr *)rbb)->h_proto) == ETH_P_IP) {
-		dump_iphdr_all((struct iphdr *)(rbb + sizeof(struct ethhdr)));
+		dump_iphdr_all((struct iphdr *)(rbb + off_n));
+		dbg("\n");
+		off_o = off_n;
+		off_n += sizeof(struct iphdr);
 
-//                /* Check for TCP */
-//                if(ntohs(((struct iphdr *) (rbb + sizeof(struct ethhdr)))->protocol) == )
-//                {
-//                        dump_tcphdr_all((struct tcphdr *) (rbb + sizeof(struct ethhdr) + sizeof(struct iphdr)));
-//                }
+		/* Check for TCP */
+		if (((struct iphdr *)(rbb + off_o))->protocol == 0x6) {
+			dump_tcphdr_all((struct tcphdr *)(rbb + off_n));
+			dbg("\n");
+			off_o = off_n;
+			off_n += sizeof(struct tcphdr);
+		}
 	}
+
+	dump_payload_all(rbb + off_n, len - off_n);
+	dbg("\n");
+
+	dbg("\n");
 }
 
 /**
@@ -193,24 +235,34 @@ void print_packet_buffer_mode_1(ring_buff_bytes_t * rbb, int len)
  */
 void print_packet_buffer_mode_2(ring_buff_bytes_t * rbb, int len)
 {
-	dbg("%d Byte\n", len);
+	size_t off_n, off_o;
 
-	/* Print proto stuff */
-	dbg("  T: 0x%02x%02x\n", rbb[12], rbb[13]);
+	dbg("%d Byte: \n", len);
 
-	/* Source host stuff */
-	dbg("  S: %02x:%02x:%02x:%02x:%02x:%02x\n",
-	    rbb[6], rbb[7], rbb[8], rbb[9], rbb[10], rbb[11]);
+	dump_ethhdr_all((struct ethhdr *)rbb);
+	dbg("\n");
+	off_n = sizeof(struct ethhdr);
 
-	/* Destination host stuff */
-	dbg("  D: %02x:%02x:%02x:%02x:%02x:%02x\n",
-	    rbb[0], rbb[1], rbb[2], rbb[3], rbb[4], rbb[5]);
+	/* Check for IP */
+	if (ntohs(((struct ethhdr *)rbb)->h_proto) == ETH_P_IP) {
+		dump_iphdr_all((struct iphdr *)(rbb + off_n));
+		dbg("\n");
+		off_o = off_n;
+		off_n += sizeof(struct iphdr);
 
-	/* Print MAC Manufacturer */
+		/* Check for TCP */
+		if (((struct iphdr *)(rbb + off_o))->protocol == 0x6) {
+			dump_tcphdr_all((struct tcphdr *)(rbb + off_n));
+			dbg("\n");
+			off_o = off_n;
+			off_n += sizeof(struct tcphdr);
+		}
+	}
 
-	/* Print TCP Flags, UDP'n'stuff */
+	dump_payload_all(rbb + off_n, len - off_n);
+	dbg("\n");
 
-	/* Check checksum --> Print if bogus */
+	dbg("\n");
 }
 
 /**
@@ -220,18 +272,32 @@ void print_packet_buffer_mode_2(ring_buff_bytes_t * rbb, int len)
  */
 void print_packet_buffer_mode_3(ring_buff_bytes_t * rbb, int len)
 {
-	dbg("%d Byte\n", len);
+	size_t off_n, off_o;
 
-	/* Print proto stuff */
-	dbg("  T: 0x%02x%02x\n", rbb[12], rbb[13]);
+	dbg("%d Byte: \n", len);
 
-	/* Source host stuff */
-	dbg("  S: %02x:%02x:%02x:%02x:%02x:%02x\n",
-	    rbb[6], rbb[7], rbb[8], rbb[9], rbb[10], rbb[11]);
+	dump_ethhdr_all((struct ethhdr *)rbb);
+	dbg("\n");
+	off_n = sizeof(struct ethhdr);
 
-	/* Destination host stuff */
-	dbg("  D: %02x:%02x:%02x:%02x:%02x:%02x\n",
-	    rbb[0], rbb[1], rbb[2], rbb[3], rbb[4], rbb[5]);
+	/* Check for IP */
+	if (ntohs(((struct ethhdr *)rbb)->h_proto) == ETH_P_IP) {
+		dump_iphdr_all((struct iphdr *)(rbb + off_n));
+		dbg("\n");
+		off_o = off_n;
+		off_n += sizeof(struct iphdr);
 
-	/* Print whole payload, readable and bytewise */
+		/* Check for TCP */
+		if (((struct iphdr *)(rbb + off_o))->protocol == 0x6) {
+			dump_tcphdr_all((struct tcphdr *)(rbb + off_n));
+			dbg("\n");
+			off_o = off_n;
+			off_n += sizeof(struct tcphdr);
+		}
+	}
+
+	dump_payload_all(rbb + off_n, len - off_n);
+	dbg("\n");
+
+	dbg("\n");
 }
