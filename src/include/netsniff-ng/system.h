888b7070dacc91c5d2b0cf52e6331adaedf299b5
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2009-12-29 17:49:31 +0000

    Added a "just in case" memset()
    git-svn-id: http://netsniff-ng.googlecode.com/svn/trunk@106 21e0ff64-9a0b-11de-825e-994487f65616

--- src/include/netsniff-ng/system.h
+++ src/include/netsniff-ng/system.h
@@ -75,6 +75,8 @@ static inline void prepare_polling(int sock, struct pollfd *pfd)
 {
 	assert(pfd);
 
+        memset(pfd, 0, sizeof(*pfd));
+        
 	pfd->fd = sock;
 	pfd->revents = 0;
 	pfd->events = POLLIN | POLLERR;
