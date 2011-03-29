2b2539f3424cbc801107f2ab56a830495e860a99
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-03-29 13:58:15 +0200

    skeletons for configurator generation scripts

---
+++ src/trafgen_imix.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+# TODO: trafgen packet distribution generator for
+#       IMIX distribution (64:7, 570:4, 1518:1)
+
---
+++ src/trafgen_tolly.pl
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+# TODO: trafgen packet distribution generator for
+#       Tolly distribution (64:55, 78:5, 576:17, 1518:23)
+
