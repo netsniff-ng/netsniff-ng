2e15d23e2ab0073dcdb8f93f3b5bb3d04a50004e
Daniel Borkmann <daniel@netsniff-ng.org>
2011-04-23 21:23:16 +0200

    methods of rw_pcap

--- src/rw_pcap.c
+++ src/rw_pcap.c
@@ -18,24 +18,72 @@
 
 static int rw_pcap_pull_file_header(int fd)
 {
+	ssize_t ret;
+	struct pcap_filehdr hdr;
+
+	ret = read(fd, &hdr, sizeof(hdr));
+	if (unlikely(ret != sizeof(hdr)))
+		return -EIO;
+	pcap_validate_header_maybe_die(&hdr);
+
 	return 0;
 }
 
 static int rw_pcap_push_file_header(int fd)
 {
+	ssize_t ret;
+	struct pcap_filehdr hdr;
+
+	memset(&hdr, 0, sizeof(hdr));
+	pcap_prepare_header(&hdr, LINKTYPE_EN10MB, 0,
+			    PCAP_DEFAULT_SNAPSHOT_LEN);
+	ret = write_or_die(fd, &hdr, sizeof(hdr));
+	if (unlikely(ret != sizeof(hdr))) {
+		whine("Failed to write pkt file header!\n");
+		return -EIO;
+	}
+
 	return 0;
 }
 
 static ssize_t rw_pcap_write_pcap_pkt(int fd, struct pcap_pkthdr *hdr,
 				      uint8_t *packet, size_t len)
 {
-	return 0;
+	ssize_t ret;
+
+	ret = write_or_die(fd, hdr, sizeof(*hdr));
+	if (unlikely(ret != sizeof(*hdr))) {
+		whine("Failed to write pkt header!\n");
+		return -EIO;
+	}
+	if (unlikely(hdr->len != len))
+		return -EINVAL;
+
+	ret = write_or_die(fd, packet, hdr->len);
+	if (unlikely(ret != hdr->len)) {
+		whine("Failed to write pkt payload!\n");
+		return -EIO;
+	}
+
+	return sizeof(*hdr) + hdr->len;
 }
 
 static ssize_t rw_pcap_read_pcap_pkt(int fd, struct pcap_pkthdr *hdr,
 				     uint8_t *packet, size_t len)
 {
-	return 0;
+	ssize_t ret;
+
+	ret = read(fd, hdr, sizeof(*hdr));
+	if (unlikely(ret != sizeof(*hdr)))
+		return -EIO;
+	if (unlikely(hdr->len > len))
+		return -ENOMEM;
+
+	ret = read(fd, packet, hdr->len);
+	if (unlikely(ret != hdr->len))
+		return -EIO;
+
+	return sizeof(*hdr) + hdr->len;
 }
 
 struct pcap_file_ops rw_pcap_ops __read_mostly = {
@@ -54,66 +102,3 @@ void cleanup_rw_pcap(void)
 {
 	pcap_ops_group_unregister(PCAP_OPS_RW);
 }
-
-#if 0
-ssize_t pcap_write_pkt(int fd, struct pcap_pkthdr *hdr,  uint8_t *packet)
-{
-	ssize_t ret;
-
-	ret = write_or_die(fd, hdr, sizeof(*hdr));
-	if (unlikely(ret != sizeof(*hdr))) {
-		whine("Failed to write pkt header!\n");
-		return -EIO;
-	}
-
-	ret = write_or_die(fd, packet, hdr->len);
-	if (unlikely(ret != hdr->len)) {
-		whine("Failed to write pkt payload!\n");
-		return -EIO;
-	}
-
-	fsync_or_die(fd, "Syncing packet buffer");
-	return (sizeof(*hdr) + hdr->len);
-}
-
-int pcap_read_still_has_packets(int fd)
-{
-	ssize_t ret;
-	off_t pos;
-	struct pcap_pkthdr hdr;
-
-	pos = lseek(fd, (off_t) 0, SEEK_CUR);
-	if (unlikely(pos < 0)) {
-		whine("Cannot seek offset of pcap file!\n");
-		return -EIO;
-	}
-
-	ret = read(fd, &hdr, sizeof(hdr));
-	if (unlikely(ret != sizeof(hdr)))
-		return 0;
-
-	if (unlikely(lseek(fd, pos + hdr.len, SEEK_SET) < 0))
-		return 0;
-	if (unlikely(lseek(fd, pos, SEEK_SET) < 0)) {
-		whine("Cannot rewind the pcap file!\n");
-		return -EIO;
-	}
-
-	return 1;
-}
-
-ssize_t pcap_read_packet(int fd, struct pcap_pkthdr *hdr, uint8_t *packet,
-			 size_t len)
-{
-	ssize_t ret;
-	ret = read(fd, hdr, sizeof(*hdr));
-	if (unlikely(ret != sizeof(*hdr)))
-		return -EIO;
-	if (unlikely(hdr->len > len))
-		return -ENOMEM;
-	ret = read(fd, packet, hdr->len);
-	if (unlikely(ret != hdr->len))
-		return -EIO;
-	return hdr->len;
-}
-#endif
