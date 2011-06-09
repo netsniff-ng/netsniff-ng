1ac953310131e0742057316adb47f9b019ac61fd
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-06-09 12:00:28 +0200

    changed comment

--- src/curves.h
+++ src/curves.h
@@ -1,7 +1,7 @@
 /*
  * netsniff-ng - the packet sniffing beast
  * By Daniel Borkmann <daniel@netsniff-ng.org>
- * Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
+ * Partial Copyright 2011 Daniel Borkmann <dborkma@tik.ee.ethz.ch>,
  * Swiss federal institute of technology (ETH Zurich)
  * Subject to the GPL.
  */
