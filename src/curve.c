0162f7901c7f5bc3befe445dd774d412b5c5c539
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-06-28 17:46:51 +0200

    hexparse

--- src/curve.c
+++ src/curve.c
@@ -187,6 +187,55 @@ static void taia_now(struct taia *t)
 	t->atto++;
 }
 
+static int hexdigit(char x)
+{
+	if (x >= '0' && x <= '9')
+		return x - '0';
+	if (x >= 'a' && x <= 'f')
+		return 10 + (x - 'a');
+	if (x >= 'A' && x <= 'F')
+		return 10 + (x - 'A');
+	return -1;
+}
+
+int curve25519_pubkey_hexparse_32(unsigned char *y, size_t ylen,
+				  const char *x, size_t len)
+{
+	int seen_digits = 0, seen_colons = 0;
+
+	if (!x || !y || ylen != 32)
+		return 0;
+
+	while (len > 0) {
+		int digit0, digit1;
+
+		if (x[0] == '\0')
+			break;
+		if (x[0] == ':') {
+			seen_colons++;
+			--len;
+			x++;
+			continue;
+		}
+
+		digit0 = hexdigit(x[0]);
+		if (digit0 == -1)
+			return 0;
+		digit1 = hexdigit(x[1]);
+		if (digit1 == -1)
+			return 0;
+
+		*y++ = digit1 + 16 * digit0;
+		seen_digits++;
+		--len;
+		x += 2;
+	}
+
+	if (x[0] != '\0' || seen_digits != 32 || seen_colons != 31)
+		return 0;
+	return 1;
+}
+
 int curve25519_alloc_or_maybe_die(struct curve25519_struct *c)
 {
 	if (!c)
