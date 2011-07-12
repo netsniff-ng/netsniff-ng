4b90e4e6530f6e99a09797c33dbfc9b1d2c83213
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-07-12 16:28:22 +0200

    whitespace

--- src/curve.h
+++ src/curve.h
@@ -74,7 +74,6 @@ extern ssize_t curve25519_decode(struct curve25519_struct *c,
 static inline void tai_pack(unsigned char *s, struct tai *t)
 {
 	uint64_t x;
-
 	x = t->x;
 	s[7] = x & 255; x >>= 8;
 	s[6] = x & 255; x >>= 8;
@@ -89,7 +88,6 @@ static inline void tai_pack(unsigned char *s, struct tai *t)
 static inline void tai_unpack(unsigned char *s, struct tai *t)
 {
 	uint64_t x;
-
 	x = (unsigned char) s[0];
 	x <<= 8; x += (unsigned char) s[1];
 	x <<= 8; x += (unsigned char) s[2];
@@ -104,16 +102,13 @@ static inline void tai_unpack(unsigned char *s, struct tai *t)
 static inline void taia_pack(unsigned char *s, struct taia *t)
 {
 	unsigned long x;
-
 	tai_pack(s, &t->sec);
 	s += 8;
-
 	x = t->atto;
 	s[7] = x & 255; x >>= 8;
 	s[6] = x & 255; x >>= 8;
 	s[5] = x & 255; x >>= 8;
 	s[4] = x;
-
 	x = t->nano;
 	s[3] = x & 255; x >>= 8;
 	s[2] = x & 255; x >>= 8;
@@ -124,16 +119,13 @@ static inline void taia_pack(unsigned char *s, struct taia *t)
 static inline void taia_unpack(unsigned char *s, struct taia *t)
 {
 	unsigned long x;
-
 	tai_unpack(s, &t->sec);
 	s += 8;
-
 	x = (unsigned char) s[4];
 	x <<= 8; x += (unsigned char) s[5];
 	x <<= 8; x += (unsigned char) s[6];
 	x <<= 8; x += (unsigned char) s[7];
 	t->atto = x;
-
 	x = (unsigned char) s[0];
 	x <<= 8; x += (unsigned char) s[1];
 	x <<= 8; x += (unsigned char) s[2];
@@ -161,17 +153,13 @@ static inline void taia_sub(struct taia *res,
 {
 	unsigned long unano = u->nano;
 	unsigned long uatto = u->atto;
-
 	res->sec.x = u->sec.x - v->sec.x;
-
 	res->nano = unano - v->nano;
 	res->atto = uatto - v->atto;
-
 	if (res->atto > uatto) {
 		res->atto += 1000000000UL;
 		--res->nano;
 	}
-
 	if (res->nano > unano) {
 		res->nano += 1000000000UL;
 		--res->sec.x;
@@ -186,15 +174,12 @@ static inline void taia_add(struct taia *res,
 			    const struct taia *v)
 {
 	res->sec.x = u->sec.x + v->sec.x;
-
 	res->nano = u->nano + v->nano;
 	res->atto = u->atto + v->atto;
-
 	if (res->atto > 999999999UL) {
 		res->atto -= 1000000000UL;
 		++res->nano;
 	}
-
 	if (res->nano > 999999999UL) {
 		res->nano -= 1000000000UL;
 		++res->sec.x;
