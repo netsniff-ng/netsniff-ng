1487bbda96e7e24b49c02016b9d1d256cb83c325
Daniel Borkmann <borkmann@gnumaniacs.org>
2011-09-08 10:44:52 +0200

    Added skeleton for LDAP data retrival

---
+++ src/curvetun_ldap.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+# curvetun_ldap.pl: generate a curvetun clients file from LDAP data
+# Copyright 2011 Daniel Borkmann <borkmann@gnumaniacs.org>
+# Subject to the GNU GPL, version 2.
+
+use warnings;
+use strict;
+
+# TODO
