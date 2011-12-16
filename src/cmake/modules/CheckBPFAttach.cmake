13f32e168db094cdc1091bb19b5b1b887ad1d9d2
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-12-16 18:39:16 +0100

    Added GPLv2 header in CMake script.

--- src/cmake/modules/CheckBPFAttach.cmake
+++ src/cmake/modules/CheckBPFAttach.cmake
@@ -1,3 +1,9 @@
+#
+# netsniff-ng - the packet sniffing beast
+# By Daniel Borkmann <daniel@netsniff-ng.org>
+# Copyright 2011 Emmanuel Roullit <emmanuel@netsniff-ng.org>.
+# Subject to the GPL, version 2.
+#
 
 INCLUDE(CheckCSourceRuns)
       
