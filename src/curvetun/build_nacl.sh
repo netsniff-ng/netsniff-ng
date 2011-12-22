a1f3e5f8b24830118d82b674a52b8ae360b6f5e1
Emmanuel Roullit <emmanuel@netsniff-ng.org>
2011-12-22 18:00:20 +0100

    Added POD documentation to NaCl build script.

--- src/curvetun/build_nacl.sh
+++ src/curvetun/build_nacl.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+<<POD
+
+=head1 NAME
+
+build_nacl - build NaCl in the wanted build directory
+
+=head1 SYNOPSIS
+
+build_nacl <nacl_build_path>
+
+=head1 DESCRIPTION
+
+This script was written to faciliate NaCl building and
+integration in the netsniff-g toolkit infrastructure.
+
+It unpacks the content of contrib/nacl/nacl-20110221.tar.bz2
+in the given build directory, builds it and output the path
+to NaCl library and its header in nacl_path.cmake.
+
+The following file is then included by CMake to find NaCl
+for the linking operation.
+
+=head1 AUTHOR
+
+Written by Emmanuel Roullit <emmanuel@netsniff-ng.org>
+
+=cut
+
+POD
+
 nacl_dir="../../contrib/nacl/"
 nacl_version="nacl-20110221"
 nacl_suffix="tar.bz2"
