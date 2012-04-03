d3ff5fcdbc7c88dd791f078b0f3d41e932f2c159
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-03 18:55:30 +0200

    ring_rx: minor: fixed ring size printout
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- src/ring_rx.c
+++ src/ring_rx.c
@@ -25,8 +25,10 @@ void destroy_rx_ring(int sock, struct ring *ring)
 	memset(&ring->layout, 0, sizeof(ring->layout));
 	setsockopt(sock, SOL_PACKET, PACKET_RX_RING, &ring->layout,
 		   sizeof(ring->layout));
+
 	munmap(ring->mm_space, ring->mm_len);
 	ring->mm_len = 0;
+
 	xfree(ring->frames);
 }
 
@@ -34,10 +36,13 @@ void setup_rx_ring_layout(int sock, struct ring *ring, unsigned int size,
 			  int jumbo_support)
 {
 	memset(&ring->layout, 0, sizeof(ring->layout));
+
 	ring->layout.tp_block_size = (jumbo_support ?
-				      getpagesize() << 4 : getpagesize() << 2);
+				      getpagesize() << 4 :
+				      getpagesize() << 2);
 	ring->layout.tp_frame_size = (jumbo_support ?
-				      TPACKET_ALIGNMENT << 12 : TPACKET_ALIGNMENT << 7);
+				      TPACKET_ALIGNMENT << 12 :
+				      TPACKET_ALIGNMENT << 7);
 	ring->layout.tp_block_nr = size / ring->layout.tp_block_size;
 	ring->layout.tp_frame_nr = ring->layout.tp_block_size /
 				   ring->layout.tp_frame_size *
@@ -61,11 +66,13 @@ retry:
 					   ring->layout.tp_block_nr;
 		goto retry;
 	}
+
 	if (ret < 0)
 		panic("Cannot allocate RX_RING!\n");
+
 	ring->mm_len = ring->layout.tp_block_size * ring->layout.tp_block_nr;
 
-	printf("RX: %.2f MB, %u Frames each %u Byte allocated\n",
+	printf("RX: %.2f MiB, %u Frames each %u Byte allocated\n",
 	       1.f * ring->mm_len / (1 << 20),
 	       ring->layout.tp_frame_nr, ring->layout.tp_frame_size);
 }
@@ -84,8 +91,10 @@ void alloc_rx_ring_frames(struct ring *ring)
 {
 	int i;
 	size_t len = ring->layout.tp_frame_nr * sizeof(*ring->frames);
+
 	ring->frames = xmalloc_aligned(len, 64);
 	memset(ring->frames, 0, len);
+
 	for (i = 0; i < ring->layout.tp_frame_nr; ++i) {
 		ring->frames[i].iov_len = ring->layout.tp_frame_size;
 		ring->frames[i].iov_base = ring->mm_space +
@@ -95,23 +104,24 @@ void alloc_rx_ring_frames(struct ring *ring)
 
 void bind_rx_ring(int sock, struct ring *ring, int ifindex)
 {
+	int ret;
 	/*
 	 * The RX_RING registers itself to the networking stack with
 	 * dev_add_pack(), so we have one single RX_RING for all devs
 	 * otherwise you'll get the packet twice.
 	 */
 	memset(&ring->s_ll, 0, sizeof(ring->s_ll));
+
 	ring->s_ll.sll_family = AF_PACKET;
 	ring->s_ll.sll_protocol = htons(ETH_P_ALL);
 	ring->s_ll.sll_ifindex = ifindex; /* Take 0 for "any"-device */
 	ring->s_ll.sll_hatype = 0;
 	ring->s_ll.sll_halen = 0;
 	ring->s_ll.sll_pkttype = 0;
-	int ret = bind(sock, (struct sockaddr *) &ring->s_ll,
-		       sizeof(ring->s_ll));
+
+	ret = bind(sock, (struct sockaddr *) &ring->s_ll, sizeof(ring->s_ll));
 	if (ret < 0) {
 		destroy_rx_ring(sock, ring);
 		panic("Cannot bind RX_RING!\n");
 	}
 }
-
