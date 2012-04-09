89c8339c48b4f1705ad0df9f47e92449f0272aa7
Christoph Jaeger <github@christophjaeger.fastmail.us>
2012-04-09 21:52:13 +0200

    Revert "dissector: disabled dissection layer 2 protocol dissection"
    This reverts commit 8e79595b28d396bea74808a62486fcdd09a9541d in order to re-enable dissection layer 2 protocol dissectors which has been disabled for modification.
    
    Signed-off-by: Christoph Jaeger <christoph@netsniff-ng.org>

--- src/protos.h
+++ src/protos.h
@@ -8,7 +8,11 @@
 #ifndef PROTOS_H
 #define PROTOS_H
 
+#include "proto_arp.h"
 #include "proto_ethernet.h"
+#include "proto_vlan.h"
+#include "proto_ipv4.h"
+#include "proto_ipv6.h"
 #include "proto_hex.h"
 
 #endif /* PROTOS_H */
