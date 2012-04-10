c4523789d23062d27a22c5c7f9b599a267d674af
Christoph Jaeger <github@christophjaeger.fastmail.us>
2012-04-10 16:06:37 +0200

    dissector: icmpv4: modified to make use of pkt_buff
    Signed-off-by: Christoph Jaeger <christoph@netsniff-ng.org>

--- src/proto_icmpv4.h
+++ src/proto_icmpv4.h
@@ -14,6 +14,7 @@
 
 #include "proto_struct.h"
 #include "dissector_eth.h"
+#include "pkt_buff.h"
 
 struct icmphdr {
 	uint8_t type;
@@ -32,11 +33,12 @@ struct icmphdr {
 	} un;
 } __attribute__((packed));
 
-static inline void icmp(uint8_t *packet, size_t len)
+static inline void icmp(struct pkt_buff *pkt)
 {
-	struct icmphdr *icmp = (struct icmphdr *) packet;
+	struct icmphdr *icmp =
+		(struct icmphdr *) pkt_pull_head(pkt, sizeof(*icmp));
 
-	if (len < sizeof(struct icmphdr))
+	if (icmp == NULL)
 		return;
 
 	tprintf(" [ ICMP ");
@@ -46,11 +48,12 @@ static inline void icmp(uint8_t *packet, size_t len)
 	tprintf(" ]\n");
 }
 
-static inline void icmp_less(uint8_t *packet, size_t len)
+static inline void icmp_less(struct pkt_buff *pkt)
 {
-	struct icmphdr *icmp = (struct icmphdr *) packet;
+	struct icmphdr *icmp =
+		(struct icmphdr *) pkt_pull_head(pkt, sizeof(*icmp));
 
-	if (len < sizeof(struct icmphdr))
+	if (icmp == NULL)
 		return;
 
 	tprintf(" Type %u Code %u", icmp->type, icmp->code);
@@ -58,10 +61,8 @@ static inline void icmp_less(uint8_t *packet, size_t len)
 
 struct protocol icmp_ops = {
 	.key = 0x01,
-	.offset = sizeof(struct icmphdr),
 	.print_full = icmp,
 	.print_less = icmp_less,
-	.proto_next = NULL,
 };
 
 #endif /* ICMP_H */
