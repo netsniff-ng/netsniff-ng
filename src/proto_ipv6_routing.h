44975e874e31928876c785e19bef4cc462154fa2
Daniel Borkmann <borkmann@iogearbox.net>
2012-04-18 19:53:07 +0200

    proto_ipv6_routing: code cleanups
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- src/proto_ipv6_routing.h
+++ src/proto_ipv6_routing.h
@@ -6,8 +6,8 @@
  * IPv6 Routing Header described in RFC2460
  */
 
-#ifndef ROUTING_H
-#define ROUTING_H
+#ifndef PROTO_IPV6_ROUTING_H
+#define PROTO_IPV6_ROUTING_H
 
 #include <stdio.h>
 #include <stdint.h>
@@ -17,7 +17,7 @@
 #include "proto_struct.h"
 #include "dissector_eth.h"
 
-#define ROUTING_HEADER_TYPE_0 0x00
+#define ROUTING_HEADER_TYPE_0	0x00
 
 struct routinghdr {
 	uint8_t h_next_header;
@@ -38,37 +38,39 @@ struct ipv6_adrr {
 	uint32_t fourth_block;
 } __attribute__((packed));
 
-static inline void dissect_routinghdr_type_0(struct pkt_buff *pkt, uint8_t *hdr_ext_len)
+static inline void dissect_routinghdr_type_0(struct pkt_buff *pkt,
+					     uint8_t *hdr_ext_len)
 {
+	uint8_t num_addr;
 	char address[INET6_ADDRSTRLEN];
 	struct ipv6_adrr *addr;
-	uint8_t num_addr = *hdr_ext_len * 8 / sizeof(* addr);
-	
-  	struct routinghdr_0 *routing_0 = (struct routinghdr_0 *) pkt_pull(pkt, sizeof(*routing_0));
-	
+	struct routinghdr_0 *routing_0;
+
+  	routing_0 = (struct routinghdr_0 *) pkt_pull(pkt, sizeof(*routing_0));
 	if (routing_0 == NULL)
 		return;
-	
+
 	tprintf("Res (%x)", routing_0->reserved);
-	
+
+	num_addr = *hdr_ext_len * 8 / sizeof(*addr);
 	while (num_addr--) {
 		addr = (struct ipv6_adrr *) pkt_pull(pkt, sizeof(*addr));
-		
 		if (addr == NULL)
 			return;
-		
-		tprintf("\n\t   Address: ");
-		tprintf("%s", inet_ntop(AF_INET6, addr,
-			address, sizeof(address)));
-		
-	}	
+
+		tprintf("\n\t   Address: %s",
+			inet_ntop(AF_INET6, addr, address,
+				  sizeof(address)));
+	}
 }
 
 static inline void routing(struct pkt_buff *pkt)
 {
 	uint8_t hdr_ext_len;
-	struct routinghdr *routing = (struct routinghdr *) pkt_pull(pkt, sizeof(*routing));
-	
+	struct routinghdr *routing;
+
+	routing = (struct routinghdr *) pkt_pull(pkt, sizeof(*routing));
+
 	/* Total Header Length in Bytes */
 	hdr_ext_len = (routing->h_hdr_ext_len + 1) * 8;
 	if (routing == NULL)
@@ -76,33 +78,35 @@ static inline void routing(struct pkt_buff *pkt)
 
 	tprintf("\t [ Routing ");
 	tprintf("NextHdr (%u), ", routing->h_next_header);
-	tprintf("HdrExtLen (%u, %u Bytes), ", routing->h_hdr_ext_len, hdr_ext_len);
+	tprintf("HdrExtLen (%u, %u Bytes), ", routing->h_hdr_ext_len,
+		hdr_ext_len);
 	tprintf("Type (%u), ", routing->h_routing_type);
 	tprintf("Left (%u), ", routing->h_segments_left);
-	
+
 	switch (routing->h_routing_type) {
-	  case ROUTING_HEADER_TYPE_0:
+	case ROUTING_HEADER_TYPE_0:
 		dissect_routinghdr_type_0(pkt, &routing->h_hdr_ext_len);
 		break;
-	  default:
+	default:
 		tprintf("Type %u is unknown", routing->h_routing_type);
 	}
 
 	tprintf(" ]\n");
-	
+
 	pkt_set_proto(pkt, &eth_lay3, routing->h_next_header);
 }
 
 static inline void routing_less(struct pkt_buff *pkt)
 {
-	struct routinghdr *routing = (struct routinghdr *) pkt_pull(pkt, sizeof(*routing));
-	
+	struct routinghdr *routing;
+
+	routing = (struct routinghdr *) pkt_pull(pkt, sizeof(*routing));
 	if (routing == NULL)
 		return;
-	
-	tprintf(" Routing ");
-	tprintf("Addresses (%u)", routing->h_hdr_ext_len * 8 / sizeof(struct ipv6_adrr));
 
+	tprintf(" Routing ");
+	tprintf("Addresses (%u)",
+		routing->h_hdr_ext_len * 8 / sizeof(struct ipv6_adrr));
 }
 
 struct protocol ipv6_routing_ops = {
@@ -111,4 +115,4 @@ struct protocol ipv6_routing_ops = {
 	.print_less = routing_less,
 };
 
-#endif /* ROUTING_H */
+#endif /* PROTO_IPV6_ROUTING_H */
