3848a8dfc7574f10542df78469976bdd7a923507
Markus Amend <markus@netsniff-ng.org>
2012-04-23 11:20:55 +0200

    proto_ip_authentication_hdr.h: smaller bugfixing

--- src/proto_ip_authentication_hdr.h
+++ src/proto_ip_authentication_hdr.h
@@ -27,17 +27,17 @@ struct auth_hdr {
 
 static inline void auth_hdr(struct pkt_buff *pkt)
 {
-	size_t hdr_payload_len;
+	size_t hdr_len;
 	struct auth_hdr *auth_ops;
 
 	auth_ops = (struct auth_hdr *) pkt_pull(pkt, sizeof(*auth_ops));
-	hdr_payload_len = (auth_ops->h_payload_len * 4) + 8;
-	if (auth_ops == NULL || hdr_payload_len > pkt_len(pkt))
+	hdr_len = (auth_ops->h_payload_len * 4) + 8;
+	if (auth_ops == NULL || hdr_len > pkt_len(pkt))
 		return;
 
 	tprintf(" [ Authentication Header ");
 	tprintf("NextHdr (%u), ", auth_ops->h_next_header);
-	tprintf("HdrLen (%u), ", hdr_payload_len);
+	tprintf("HdrLen (%u), ", hdr_len);
 	tprintf("Reserved (0x%x), ", ntohs(auth_ops->h_reserved));
 	/* TODO
 	 * Upgrade for Extended (64-bit) Sequence Number
@@ -46,7 +46,7 @@ static inline void auth_hdr(struct pkt_buff *pkt)
 	tprintf("SPI (0x%x), ", ntohl(auth_ops->h_spi));
 	tprintf("SNF (0x%x), ", ntohl(auth_ops->h_snf));
 	tprintf("ICV 0x");
-	for (size_t i = sizeof(struct auth_hdr); i < hdr_payload_len; i++)
+	for (size_t i = sizeof(struct auth_hdr); i < hdr_len; i++)
 		tprintf("%02x", *pkt_pull(pkt, 1));
 	tprintf(" ]\n");
 
@@ -55,17 +55,18 @@ static inline void auth_hdr(struct pkt_buff *pkt)
 
 static inline void auth_hdr_less(struct pkt_buff *pkt)
 {
-  	size_t hdr_payload_len;
+  	size_t hdr_len;
 	struct auth_hdr *auth_ops;
 
 	auth_ops = (struct auth_hdr *) pkt_pull(pkt, sizeof(*auth_ops));
-	hdr_payload_len = (auth_ops->h_payload_len * 4) + 8;
-	if (auth_ops == NULL || hdr_payload_len > pkt_len(pkt))
+	hdr_len = (auth_ops->h_payload_len * 4) + 8;
+	if (auth_ops == NULL || hdr_len > pkt_len(pkt))
 		return;
 
 	tprintf(" AH");
 
-	pkt_set_proto(pkt, &eth_lay3, hdr_payload_len);
+	pkt_pull(pkt, hdr_len - sizeof(*auth_ops));
+	pkt_set_proto(pkt, &eth_lay3, auth_ops->h_next_header);
 }
 
 struct protocol ip_auth_hdr_ops = {
