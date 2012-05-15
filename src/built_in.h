dc2971b4a3839bd04b30825556d340b825df822b
Daniel Borkmann <borkmann@iogearbox.net>
2012-05-15 14:40:06 +0200

    built_in: add kernel u* types
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- src/built_in.h
+++ src/built_in.h
@@ -246,4 +246,9 @@ static inline uint64_t ntohll(uint64_t x)
 # error __BYTE_ORDER is neither __LITTLE_ENDIAN nor __BIG_ENDIAN
 #endif
 
+typedef uint64_t	u64;
+typedef uint32_t	u32;
+typedef uint16_t	u16;
+typedef uint8_t		u8;
+
 #endif /* BUILT_IN_H */
