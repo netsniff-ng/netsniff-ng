3ae502304d296adc17aa56ae91b539d9ece47229
Markus Amend <markus@netsniff-ng.org>
2012-05-16 17:46:12 +0200

    proto_mpls_unicast.c: Upper Layer recognizing

--- src/proto_mpls_unicast.c
+++ src/proto_mpls_unicast.c
@@ -13,16 +13,19 @@
 #include "proto.h"
 #include "protos.h"
 #include "dissector_eth.h"
+#include "built_in.h"
 #include "pkt_buff.h"
 
 struct mpls_uchdr {
 	uint32_t mpls_uc_hdr;
-} __attribute__((packed));
+} __packed;
 
 static void mpls_uc_full(struct pkt_buff *pkt)
 {
-	uint8_t s = 0;
+	uint8_t proto;
+	unsigned int key;
 	uint32_t mpls_uc_data;
+	uint8_t s = 0;
 
 	do {
 		struct mpls_uchdr *mpls_uc = (struct mpls_uchdr *) pkt_pull(pkt,
@@ -38,27 +41,66 @@ static void mpls_uc_full(struct pkt_buff *pkt)
 		tprintf("Label (%u), ", mpls_uc_data >> 12);
 		tprintf("Exp (%u), ", (mpls_uc_data >> 9) & 0x7);
 		tprintf("S (%u), ", s);
-		tprintf("TTL (%u), ", (mpls_uc_data & 0xFF));
+		tprintf("TTL (%u)", (mpls_uc_data & 0xFF));
 		tprintf(" ]\n");
-	}while(!s);
+	} while (!s);
 
-// 	pkt_set_proto(pkt, &eth_lay2, ntohs(mpls_uc->TPID));
+	if(pkt_len(pkt))
+	  proto = *(pkt->data);
+	else
+	  return;
+
+	switch (proto >> 4) {
+	case 4:
+	    key = 0x800; /* IPv4*/
+	    break;
+	case 6:
+	    key = 0x86DD; /* IPv6*/
+	    break;
+	default:
+	    key = 0;
+	}
+
+	pkt_set_proto(pkt, &eth_lay2, key);
 }
 
 static void mpls_uc_less(struct pkt_buff *pkt)
 {
-// 	uint16_t mpls_uc_data;
-// 	struct mpls_uchdr *mpls_uc = (struct mpls_uchdr *) pkt_pull(pkt,
-// sizeof(*mpls_uc));
-// 
-// 	if (mpls_uc == NULL)
-// 		return;
-// 
-// 	mpls_uc_data = ntohs(mpls_uc->TCI);
-// 
-// 	tprintf(" VLAN%d", (mpls_uc_data & 0x0FFF));
-// 
-// 	pkt_set_proto(pkt, &eth_lay2, ntohs(mpls_uc->TPID));
+	uint8_t proto;
+	unsigned int key;
+	uint32_t mpls_uc_data;
+	uint8_t s = 0;
+
+	do {
+		struct mpls_uchdr *mpls_uc = (struct mpls_uchdr *) pkt_pull(pkt,
+							      sizeof(*mpls_uc));
+
+		if (mpls_uc == NULL)
+			return;
+
+		mpls_uc_data = ntohl(mpls_uc->mpls_uc_hdr);
+		s = (mpls_uc_data >> 8) & 0x1;
+
+		tprintf(" MPLS/%u", mpls_uc_data >> 12);
+	}while(!s);
+
+	if(pkt_len(pkt))
+	  proto = *(pkt->data);
+	else
+	  return;
+
+	switch (proto >> 4) {
+	case 4:
+	    key = 0x800; /* IPv4*/
+	    break;
+	case 6:
+	    key = 0x86DD; /* IPv6*/
+	    break;
+	default:
+	    key = 0;
+	}
+
+	pkt_set_proto(pkt, &eth_lay2, key);
 }
 
 struct protocol mpls_uc_ops = {
