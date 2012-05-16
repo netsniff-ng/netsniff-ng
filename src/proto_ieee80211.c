d1ad07851e053c5093096bb19556d08ad7c00428
Daniel Borkmann <borkmann@iogearbox.net>
2012-05-16 12:00:08 +0200

    ieee80211: continuing work on frame-ctl + duration
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- src/proto_ieee80211.c
+++ src/proto_ieee80211.c
@@ -1,12 +1,14 @@
 /*
  * netsniff-ng - the packet sniffing beast
  * By Daniel Borkmann <daniel@netsniff-ng.org>
- * Copyright 2009, 2010 Daniel Borkmann.
+ * Copyright 2012 Daniel Borkmann.
  * Subject to the GPL, version 2.
  */
 
 #include <stdio.h>
 #include <stdint.h>
+#include <netinet/in.h>    /* for ntohs() */
+#include <asm/byteorder.h>
 
 #include "proto.h"
 #include "protos.h"
@@ -15,27 +17,41 @@
 #include "pkt_buff.h"
 #include "oui.h"
 
-struct ieee80211hdr {
-	u16 frame_control;
-	u16 duration;
-/*	u8 da[6];
-	u8 sa[6];
-	u8 bssid[6];
-	u16 seq_ctrl;*/
-} __packed;
+/* XXX: clarify which endianess */
 
-/* TODO: fix lots of things analyze frame control */
-/* this is really just a simple start */
-/* FIXME: duration, sequence number, addresses... */
-/* Look @:
- * http://www.rhyshaden.com/wireless.htm
- * http://technet.microsoft.com/en-us/library/cc757419(v=ws.10).aspx
- * http://www.ieee802.org/
- * PHY Sublayer (FH, DSSS etc) is not relevant, because it's physical :-)
- *
- * How to make sure which protocol is included (in unencrypted mode)? Only
- * IPv4/6?
- */
+struct ieee80211hdr {
+//	union {
+//		u16 frame_control;
+#if defined(__BIG_ENDIAN_BITFIELD)
+		__extension__ u16 subtype:4,
+				  type:2,
+				  proto_version:2,
+				  order:1,
+				  wep:1,
+				  more_data:1,
+				  power_mgmt:1,
+				  retry:1,
+				  more_frags:1,
+				  from_ds:1,
+				  to_ds:1;
+#elif defined(__LITTLE_ENDIAN_BITFIELD)
+		__extension__ u16 proto_version:2,
+				  type:2,
+				  subtype:4,
+				  to_ds:1,
+				  from_ds:1,
+				  more_frags:1,
+				  retry:1,
+				  power_mgmt:1,
+				  more_data:1,
+				  wep:1,
+				  order:1;
+#else
+# error  "Adjust your <asm/byteorder.h> defines"
+#endif
+//	};
+	u16 duration;
+} __packed;
 
 static void ieee80211(struct pkt_buff *pkt)
 {
@@ -44,25 +60,21 @@ static void ieee80211(struct pkt_buff *pkt)
 	if (hdr == NULL)
 		return;
 
-	tprintf(" [ 802.11 ");
-	/* FIXME: See above */
-	/*tprintf("MAC (%.2x:%.2x:%.2x:%.2x:%.2x:%.2x => ",
-		hdr->sa[0], hdr->sa[1], hdr->sa[2],
-		hdr->sa[3], hdr->sa[4], hdr->sa[5]);
-	tprintf("%.2x:%.2x:%.2x:%.2x:%.2x:%.2x via BSSID ",
-		hdr->da[0], hdr->da[1], hdr->da[2],
-		hdr->da[3], hdr->da[4], hdr->da[5]);
-	tprintf("%.2x:%.2x:%.2x:%.2x:%.2x:%.2x), ",
-		hdr->bssid[0], hdr->bssid[1], hdr->bssid[2],
-		hdr->bssid[3], hdr->bssid[4], hdr->bssid[5]);*/
-	tprintf("Frame Control (%x), Duration (%x)",
-		hdr->frame_control, hdr->duration);
+	tprintf(" [ 802.11 Frame Control (0x%x), Duration/ID (%u) ]\n",
+		0/*ntohs(hdr->frame_control)*/, /*ntohs(*/hdr->duration);
+	tprintf("\t [ Proto Version (%u), ", hdr->proto_version);
+	tprintf("Type (%u), ", hdr->type /*XXX*/);
+	tprintf("Subtype (%u)", hdr->subtype /*XXX*/);
+	tprintf("%s%s",
+		hdr->to_ds ? ", Frame goes to DS" : "",
+		hdr->from_ds ?  ", Frame comes from DS" : "");
+	tprintf("%s", hdr->more_frags ? ", More Fragments" : "");
+	tprintf("%s", hdr->retry ? ", Frame is retransmitted" : "");
+	tprintf("%s", hdr->power_mgmt ? ", In Power Saving Mode" : "");
+	tprintf("%s", hdr->more_data ? ", More Data" : "");
+	tprintf("%s", hdr->wep ? ", Needs WEP" : "");
+	tprintf("%s", hdr->order ? ", Order" : "");
 	tprintf(" ]\n");
-
-	/*tprintf(" [ BSS Vendor ");
-	tprintf("(%s)", lookup_vendor((hdr->bssid[0] << 16) |
-				      (hdr->bssid[1] << 8) | hdr->bssid[2]));
-	tprintf(" ]\n");*/
 
 //	pkt_set_proto(pkt, &ieee802_lay2, ntohs(eth->h_proto));
 }
