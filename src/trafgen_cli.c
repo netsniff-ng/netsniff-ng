62f40a28549df9522ae903f68268e5fa83d22f2e
Daniel Borkmann <daniel@gnumaniacs.org>
2012-06-27 13:59:12 +0200

    trafgen_cli: add stubs, to be done later
    Signed-off-by: Daniel Borkmann <daniel@gnumaniacs.org>

--- src/trafgen_cli.c
+++ src/trafgen_cli.c
@@ -7,10 +7,10 @@ int main_loop_interactive(struct mode *mode, char *confname)
 	struct cli_def *cli;
 
 	cli = cli_init();
-	cli_set_banner(cli, "libcli test environment");
-	cli_set_hostname(cli, "router");
+	cli_set_banner(cli, "trafgen interactive mode");
+	cli_set_hostname(cli, "txfnode");
 //	cli_telnet_protocol(cli, 1);
-
 	cli_done(cli);
+
 	return 0;
 }
