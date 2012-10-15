b193c53566334a10f0273258195beed78ee0524a
Daniel Borkmann <borkmann@iogearbox.net>
2012-10-15 13:33:37 +0200

    die: apply checked format string, make const
    Signed-off-by: Daniel Borkmann <borkmann@iogearbox.net>

--- src/die.h
+++ src/die.h
@@ -16,6 +16,19 @@
 #include <unistd.h>
 #include <syslog.h>
 
+#include "built_in.h"
+
+static inline void panic(const char *format, ...)  __check_format_printf(1, 2);
+static inline void syslog_panic(const char *format,
+				...) __check_format_printf(1, 2);
+static inline void syslog_maybe(int may, int priority,
+				const char *format, ...) __check_format_printf(3, 4);
+static inline void whine(const char *format, ...) __check_format_printf(1, 2);
+static inline void verbose_l1(const char *format,
+			      ...) __check_format_printf(1, 2);
+static inline void verbose_l2(const char *format,
+			      ...) __check_format_printf(1, 2);
+
 static inline void die(void)
 {
 	exit(EXIT_FAILURE);
@@ -26,37 +39,69 @@ static inline void _die(void)
 	_exit(EXIT_FAILURE);
 }
 
-static inline void panic(char *format, ...)
+static inline void panic(const char *format, ...)
 {
 	va_list vl;
+
 	va_start(vl, format);
 	vfprintf(stderr, format, vl);
 	va_end(vl);
+
 	die();
 }
 
 static inline void syslog_panic(const char *format, ...)
 {
 	va_list vl;
+
 	va_start(vl, format);
 	vsyslog(LOG_ERR, format, vl);
 	va_end(vl);
+
 	die();
 }
 
 static inline void syslog_maybe(int may, int priority, const char *format, ...)
 {
-	if (may) {	
+	if (!!may) {	
 		va_list vl;
+
 		va_start(vl, format);
 		vsyslog(priority, format, vl);
 		va_end(vl);
 	}
 }
 
-static inline void whine(char *format, ...)
+static inline void whine(const char *format, ...)
 {
 	va_list vl;
+
+	va_start(vl, format);
+	vfprintf(stderr, format, vl);
+	va_end(vl);
+}
+
+extern int verbose_level;
+
+static inline void verbose_l1(const char *format, ...)
+{
+	va_list vl;
+
+	if (verbose_level < 1)
+		return;
+
+	va_start(vl, format);
+	vfprintf(stderr, format, vl);
+	va_end(vl);
+}
+
+static inline void verbose_l2(const char *format, ...)
+{
+	va_list vl;
+
+	if (verbose_level < 2)
+		return;
+
 	va_start(vl, format);
 	vfprintf(stderr, format, vl);
 	va_end(vl);
