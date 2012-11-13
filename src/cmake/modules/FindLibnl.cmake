51291a9fd6d1486ea317dfeb2cc2e1b9699c5881
Jim Binder <jbinder6342@gmail.com>
2012-11-14 00:08:57 +0100

    cmake: fix libnl issue

--- src/cmake/modules/FindLibnl.cmake
+++ src/cmake/modules/FindLibnl.cmake
@@ -10,9 +10,14 @@
 #  LIBNL_LIBRARIES   - the libnl library
 #  LIBNL_INCLUDE_DIR - the include path of the libnl library
 
-find_library(LIBNL_LIBRARY nl-genl-3)
+find_library(LIBNL_LIBRARY nl-3)
+find_library(LIBNL_GENL_LIBRARY nl-genl-3)
+
+message(STATUS LIBNL_LIBRARY=${LIBNL_LIBRARY})
+message(STATUS LIBNL_GENL_LIBRARY=${LIBNL_GENL_LIBRARY})
 
 set(LIBNL_LIBRARIES ${LIBNL_LIBRARY})
+set(LIBNL_GENL_LIBRARIES ${LIBNL_GENL_LIBRARY})
 
 find_path(
   LIBNL_INCLUDE_DIR
@@ -20,15 +25,20 @@ find_path(
   PATH_SUFFIXES include/libnl3
 )
 
+include_directories(AFTER ${LIBNL_INCLUDE_DIR})
+message(STATUS LIBNL_INCLUDE_DIR2=${LIBNL_INCLUDE_DIR})
+
 include(FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args(
-  Libnl DEFAULT_MSG
-  LIBNL_LIBRARY
-  LIBNL_INCLUDE_DIR
+   Libnl DEFAULT_MSG
+   LIBNL_LIBRARY
+   LIBNL_INCLUDE_DIR
 )
 
 mark_as_advanced(
-  LIBNL_INCLUDE_DIR
-  LIBNL_LIBRARY
+   LIBNL_INCLUDE_DIR
+   LIBNL_LIBRARY
 )
+
+
