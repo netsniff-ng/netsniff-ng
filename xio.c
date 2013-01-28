02cb790b9e346032e8d4112b5d3dac9e01941df4
Daniel Borkmann <dborkman@redhat.com>
2013-01-28 17:55:43 +0100

    xio: add nacl's randombyte function
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>

--- xio.c
+++ xio.c
@@ -156,3 +156,44 @@ ssize_t write_exact(int fd, void *buf, size_t len, int mayexit)
 
 	return num;
 }
+
+static int fd_rnd = -1;
+
+static void randombytes(unsigned char *x, unsigned long long xlen)
+{
+	int ret;
+
+	if (fd_rnd < 0) {
+		for (;;) {
+			fd_rnd = open("/dev/urandom", O_RDONLY);
+			if (fd_rnd < 0)
+				break;
+			sleep(1);
+		}
+	}
+
+	while (xlen > 0) {
+		if (xlen < 1048576)
+			ret = xlen;
+		else
+			ret = 1048576;
+
+		ret = read(fd_rnd, x, ret);
+		if (ret < 1) {
+			sleep(1);
+			continue;
+		}
+
+		x += ret;
+		xlen -= ret;
+	}
+}
+
+int secrand(void)
+{
+	int ret;
+
+	randombytes((void *) &ret, sizeof(ret));
+
+	return ret;
+}
