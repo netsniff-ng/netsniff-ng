b938bb5ee69046812c802f41614078961c9add8a
Daniel Borkmann <dborkman@redhat.com>
2013-01-28 19:13:06 +0100

    xio: add comment to function
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>

--- xio.c
+++ xio.c
@@ -189,6 +189,10 @@ static void randombytes(unsigned char *x, unsigned long long xlen)
 	}
 }
 
+/* Note: it's not really secure, but the name only suggests it's better to use
+ * than rand(3) when transferring bytes over the network in non-security
+ * critical structure members. secrand() is only used to fill up salts actually.
+ */
 int secrand(void)
 {
 	int ret;
