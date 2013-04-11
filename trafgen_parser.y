netsniff-ng/netsniff-ng

commit f9591eebe40c9d898483c83c6d4d23d553ec43fc

trafgen: lexer/parser: fix cpu() selection and whitespacing
It looks unintuitive when once needs to write cpu(0:3), so also allow
cpu(0-3). Then it's quite stupid to enforce whitespace after the double
colon before the curley braces open. So make this rather optional here.

Signed-off-by: Daniel Borkmann <dborkman@redhat.com>

trafgen_parser.y
@@ -344,12 +344,22 @@ inline_comment
 	: K_COMMENT { }
 	;
 
+cpu_delim
+	: ':' { }
+	| '-' { }
+	;
+
+noenforce_white
+	: { }
+	| K_WHITE { }
+	;
+
 packet
 	: '{' delimiter payload delimiter '}' {
 			min_cpu = max_cpu = -1;
 			realloc_packet();
 		}
-	| K_CPU '(' number ':' number ')' ':' K_WHITE '{' delimiter payload delimiter '}' {
+	| K_CPU '(' number cpu_delim number ')' ':' noenforce_white '{' delimiter payload delimiter '}' {
 			min_cpu = $3;
 			max_cpu = $5;
 
@@ -362,7 +372,7 @@ packet
 
 			realloc_packet();
 		}
-	| K_CPU '(' number ')' ':' K_WHITE '{' delimiter payload delimiter '}' {
+	| K_CPU '(' number ')' ':' noenforce_white '{' delimiter payload delimiter '}' {
 			min_cpu = max_cpu = $3;
 			realloc_packet();
 		}

trafgen_lexer.l
@@ -72,7 +72,7 @@ static char *try_convert_shellcode(char *sstr)
 number_oct	([0][0-9]+)
 number_hex	([0][x][a-fA-F0-9]+)
 number_bin	([0][b][0-1]+)
-number_dec	(([0])|([-+]?[1-9][0-9]*))
+number_dec	(([0])|([1-9][0-9]*))
 number_ascii	([a-zA-Z])
 
 %%
@@ -120,7 +120,7 @@ number_ascii	([a-zA-Z])
 "\""[^\"]+"\""	{ yylval.str = try_convert_shellcode(xstrdup(yytext));
 		  return string; }
 
-([ \t\r\n]+)?	{ return K_WHITE; }
+([ \t\n]+)?	{ return K_WHITE; }
 
 "/*"([^\*]|\*[^/])*"*/" { return K_COMMENT; }
 

commit 2bce4249a8e90c67a24e096f91047795f92ab548

trafgen: parser: fix regression on last pkt element
Jon Schipp reported that a arbitrary packet config
such as ...

{
 "MyPacket",
 "MyPacket",
 "MyPacket",
 "MyPacket",
 "MyPacket",
 "MyPacket",
 "MyPacket",
 "MyPacket",
}

... doesn't work anymore, although it used to work. This
regression was introduced by commit f9591eebe40c ("trafgen:
lexer/parser: fix cpu() selection and whitespacing").

Reported-by: Jon Schipp <jonschipp@gmail.com>
Signed-off-by: Daniel Borkmann <dborkman@redhat.com>

trafgen_parser.y
@@ -349,9 +349,15 @@ cpu_delim
 	| '-' { }
 	;
 
+delimiter_nowhite
+	: ',' { }
+	| ',' K_WHITE { }
+	;
+
 noenforce_white
 	: { }
 	| K_WHITE { }
+	| delimiter_nowhite { }
 	;
 
 packet
@@ -384,9 +390,8 @@ payload
 	;
 
 delimiter
-	: ',' { }
+	: delimiter_nowhite { }
 	| K_WHITE { }
-	| ',' K_WHITE { }
 	;
 
 elem_delimiter