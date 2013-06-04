6c1b0eb3e6cc5a7e6f24116e386d43354c6eb98c
Daniel Borkmann <dborkman@redhat.com>
2013-06-04 14:55:40 +0200

    curve: also really remove taia functions from here
    We should also remove those functions! Arg. While at it, slightly clean
    up a bit.
    
    Signed-off-by: Daniel Borkmann <dborkman@redhat.com>

--- curve.h
+++ curve.h
@@ -1,14 +1,5 @@
-/*
- * netsniff-ng - the packet sniffing beast
- * Copyright 2011 - 2013 Daniel Borkmann.
- * Subject to the GPL, version 2.
- */
-
 #ifndef CURVE_H
 #define CURVE_H
-
-#include <stdint.h>
-#include <sys/time.h>
 
 #include "locking.h"
 #include "built_in.h"
@@ -17,12 +8,6 @@
 #include "taia.h"
 #include "crypto.h"
 
-static struct taia tolerance_taia = {
-	.sec.x = 0,
-	.nano = 700000000ULL,
-	.atto = 0,
-};
-
 struct curve25519_proto {
 	unsigned char enonce[crypto_box_noncebytes] __aligned_16;
 	unsigned char dnonce[crypto_box_noncebytes] __aligned_16;
@@ -30,12 +15,9 @@ struct curve25519_proto {
 };
 
 struct curve25519_struct {
-	unsigned char *enc_buf;
-	unsigned char *dec_buf;
-	size_t enc_buf_size;
-	size_t dec_buf_size;
-	struct spinlock enc_lock;
-	struct spinlock dec_lock;
+	unsigned char *enc_buf, *dec_buf;
+	size_t enc_buf_size, dec_buf_size;
+	struct spinlock enc_lock, dec_lock;
 };
 
 extern void curve25519_selftest(void);
@@ -49,27 +31,5 @@ extern ssize_t curve25519_encode(struct curve25519_struct *curve, struct curve25
 extern ssize_t curve25519_decode(struct curve25519_struct *curve, struct curve25519_proto *proto,
 				 unsigned char *chipertext, size_t size, unsigned char **plaintext,
 				 struct taia *arrival_taia);
-
-static inline int is_good_taia(struct taia *arrival_taia, struct taia *packet_taia)
-{
-	int is_ts_good = 0;
-	struct taia sub_res;
-
-	if (taia_less(arrival_taia, packet_taia)) {
-		taia_sub(&sub_res, packet_taia, arrival_taia);
-		if (taia_less(&sub_res, &tolerance_taia))
-			is_ts_good = 1;
-		else
-			is_ts_good = 0;
-	} else {
-		taia_sub(&sub_res, arrival_taia, packet_taia);
-		if (taia_less(&sub_res, &tolerance_taia))
-			is_ts_good = 1;
-		else
-			is_ts_good = 0;
-	}
-
-	return is_ts_good;
-}
 
 #endif /* CURVE_H */
