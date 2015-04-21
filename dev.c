netsniff-ng/netsniff-ng

commit f43bbe9e895a05a71ebfa5fb676f068621bd5014

mac80211: Check existence of generated monX device
Fix then case when netsniff-ng fails if there is already an existing
monX device while generating one.

Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

dev.c
@@ -13,7 +13,7 @@
 #include "link.h"
 #include "built_in.h"
 
-int device_ifindex(const char *ifname)
+int device_ifindex_get(const char *ifname)
 {
 	int ret, sock, index;
 	struct ifreq ifr;
@@ -27,11 +27,20 @@ int device_ifindex(const char *ifname)
 	strlcpy(ifr.ifr_name, ifname, IFNAMSIZ);
 
 	ret = ioctl(sock, SIOCGIFINDEX, &ifr);
-	if (unlikely(ret))
-		panic("Cannot get ifindex from device!\n");
+	if (ret)
+		return -1;
 
 	index = ifr.ifr_ifindex;
 	close(sock);
+
+	return index;
+}
+
+int device_ifindex(const char *ifname)
+{
+	int index = device_ifindex_get(ifname);
+	if (unlikely(index <= 0))
+		panic("Cannot get ifindex from device!\n");
 
 	return index;
 }

dev.h
@@ -7,6 +7,7 @@
 extern size_t device_mtu(const char *ifname);
 extern int device_address(const char *ifname, int af, struct sockaddr_storage *ss);
 extern int device_ifindex(const char *ifname);
+extern int device_ifindex_get(const char *ifname);
 extern int device_type(const char *ifname);
 extern short device_get_flags(const char *ifname);
 extern void device_set_flags(const char *ifname, const short flags);

mac80211.c
@@ -226,6 +226,10 @@ void enter_rfmon_mac80211(const char *device, char **mondev)
 		char mondevice[32];
 
 		slprintf(mondevice, sizeof(mondevice), "mon%u", n);
+
+		if (device_ifindex_get(mondevice) > 0)
+			continue;
+
 		ret = nl80211_add_mon_if(&nlstate, device, mondevice);
 		if (ret == 0) {
 			*mondev = xstrdup(mondevice);

commit 42ca7e42aa76ee52499ae82370d11d044e811f35

netsniff-ng: fix snooping on any device when no option is given
Commit f43bbe9e895a ("mac80211: Check existence of generated monX
device") broke starting netsniff-ng w/o any arguments, that is,
sniffing on "any" device. The test in device_ifindex() should be
index < 0.

Fixes: f43bbe9e895a ("mac80211: Check existence of generated monX device")
Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

dev.c
@@ -39,7 +39,8 @@ int device_ifindex_get(const char *ifname)
 int device_ifindex(const char *ifname)
 {
 	int index = device_ifindex_get(ifname);
-	if (unlikely(index <= 0))
+
+	if (unlikely(index < 0))
 		panic("Cannot get ifindex from device!\n");
 
 	return index;