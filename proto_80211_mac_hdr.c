netsniff-ng/netsniff-ng

commit 9278bb65e810156ed1074e693fce14e7ecf145e8

netsniff-ng: Dump basic radiotap header info
Print the basic radiotap header information in the 80211_mac_hdr
dissector.

Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
[tklauser: removed printing of binary representation of flags]
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

proto_80211_mac_hdr.c
@@ -3155,6 +3155,11 @@ static void ieee80211(struct pkt_buff *pkt)
 		rtap = (struct ieee80211_radiotap_header *)pkt_pull(pkt,
 				sizeof(*rtap));
 
+		tprintf(" [ Radiotap ");
+		tprintf("Version (%u), ", rtap->version);
+		tprintf("Length (%u), ", le16_to_cpu(rtap->len));
+		tprintf("Flags (0x%08x) ]\n", le32_to_cpu(rtap->present));
+
 		pkt_pull(pkt, le16_to_cpu(rtap->len) - sizeof(*rtap));
 	}
 

commit fa6e21e9393f814f9656460aeaa288dc543d6cec

dissectors: 80211_mac_hdr: Check return value of pkt_pull
Prevent a NULL pointer dereference if (for whatever reason) pkt_pull
returns NULL.

This issue was discovered using the Coverity scanner.

Fixes: 9278bb65 ("netsniff-ng: Dump basic radiotap header info")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

proto_80211_mac_hdr.c
@@ -3154,6 +3154,8 @@ static void ieee80211(struct pkt_buff *pkt)
 
 		rtap = (struct ieee80211_radiotap_header *)pkt_pull(pkt,
 				sizeof(*rtap));
+		if (rtap == NULL)
+			return;
 
 		tprintf(" [ Radiotap ");
 		tprintf("Version (%u), ", rtap->version);