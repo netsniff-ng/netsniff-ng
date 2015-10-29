a81b515484828e0cff848fb7689d4a4b77f020d8
Tobias Klauser <tklauser@distanz.ch>
2015-10-29 09:33:40 +0100

    zsh: flowtop: Add completion for newly added command line options
    Update the zsh completion with option -n/--no-dns, -G/--no-geoip,
    -b/--bits, -t/--interval.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- flowtop.zsh
+++ flowtop.zsh
@@ -18,8 +18,12 @@ _arguments -s -S \
     "(-D --dccp)"{-D,--dccp}"[Show only DCCP flows]" \
     "(-I --icmp)"{-I,--icmp}"[Show only ICMP/ICMPv6 flows]" \
     "(-S --sctp)"{-S,--sctp}"[Show only SCTP flows]" \
+    "(-n --no-dns)"{-n,--no-dns}"[Don't perform hostname lookup]" \
+    "(-G --no-geoip)"{-G,--no-geoip}"[Don't perform GeoIPlookup]" \
     "(-s --show-src)"{-s,--show-src}"[Also show source, not only dest]" \
+    "(-b --bits)"{-b,--bits}"[Show rates in bits/s instead of bytes/s]" \
     "(-u --update)"{-u,--update}"[Update GeoIP databases]" \
+    "(-t --interval)"{-t,--interval}"[Refresh time in seconds (def: 1s)]:interval:_gnu_generic" \
     {-v,--version}"[Print version and exit]:" \
     {-h,--help}"[Print help and exit]:" \
     "*::args:_gnu_generic"
