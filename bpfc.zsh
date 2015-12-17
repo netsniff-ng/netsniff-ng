205ca73cd1b724795f7ec295e85fbb50cbbb430b
Tobias Klauser <tklauser@distanz.ch>
2015-12-17 13:41:05 +0100

    zsh: bpfc: Add completion for -D/--define
    Add zsh completion for -D/--define command line option added in commit
    121119215276 ("bpfc: Add option to pass macro/define for C
    preprocessor")
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- bpfc.zsh
+++ bpfc.zsh
@@ -13,6 +13,7 @@ typeset -A opt_args
 _arguments -s -S \
     "(-i --input)"{-i,--input}"[Berkeley Packet Filter file]:input:_files" \
     "(-p --cpp)"{-p,--cpp}"[Run bpf program through C preprocessor]" \
+    "(-D --define)"{-D,--define}"[Add macro definition for the C preprocessor]::" \
     "(-f --format)"{-f,--format}"[Output format]:output:(C netsniff-ng xt_bpf tcpdump)" \
     "(-b --bypass)"{-b,--bypass}"[Bypass filter validation (e.g. for bug testing)]" \
     "(-d --dump)"{-d,--dump}"[Dump supported instruction table]" \
