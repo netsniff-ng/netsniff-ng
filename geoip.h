netsniff-ng/netsniff-ng

commit f61f39d3972960d795d3d5e5fe6e16179591e2bb

geoip: Allow to get country 3-code
Add func to get country code-3 name by IPv4/6 address

Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

geoip.h
@@ -17,6 +17,8 @@ extern const char *geoip4_region_name(struct sockaddr_in *sa);
 extern const char *geoip6_region_name(struct sockaddr_in6 *sa);
 extern const char *geoip4_country_name(struct sockaddr_in *sa);
 extern const char *geoip6_country_name(struct sockaddr_in6 *sa);
+extern const char *geoip4_country_code3_name(struct sockaddr_in *sa);
+extern const char *geoip6_country_code3_name(struct sockaddr_in6 *sa);
 extern float geoip4_longitude(struct sockaddr_in *sa);
 extern float geoip4_latitude(struct sockaddr_in *sa);
 extern float geoip6_longitude(struct sockaddr_in6 *sa);

geoip.c
@@ -339,6 +339,20 @@ const char *geoip6_country_name(struct sockaddr_in6 *sa)
 	return GeoIP_country_name_by_ipnum_v6(gi6_country, sa->sin6_addr);
 }
 
+const char *geoip4_country_code3_name(struct sockaddr_in *sa)
+{
+	bug_on(gi4_country == NULL);
+
+	return GeoIP_country_code3_by_ipnum(gi4_country, ntohl(sa->sin_addr.s_addr));
+}
+
+const char *geoip6_country_code3_name(struct sockaddr_in6 *sa)
+{
+	bug_on(gi6_country == NULL);
+
+	return GeoIP_country_code3_by_ipnum_v6(gi6_country, sa->sin6_addr);
+}
+
 static int fdout, fderr;
 
 /* GeoIP people were too stupid to come to the idea that you could set

commit 6b7f7cc3ff9590caa858808fb47f0a82ad17f8fc

flowtop: Fix compilation error when build without geoip
Commit f61f39d ("geoip: Allow to get country 3-code") added
new helpers without considering HAVE_GEOIP definition.

Fixed by adding dummy functions for get 3-code country name
in case if HAVE_GEOIP is not defined.

Tested only by manualy disabling HAVE_GEOIP in config.h
and geoip.c compilation in Config file.

Fixes: f61f39d (geoip: Allow to get country 3-code)
Reported-by: Daniel Borkmann <daniel@iogearbox.net>
Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

geoip.h
@@ -104,6 +104,16 @@ static inline const char *geoip6_as_name(struct sockaddr_in6 *sa)
 {
 	return NULL;
 }
+
+static inline const char *geoip4_country_code3_name(struct sockaddr_in *sa)
+{
+	return NULL;
+}
+
+static inline const char *geoip6_country_code3_name(struct sockaddr_in6 *sa)
+{
+	return NULL;
+}
 #endif
 
 #endif /* GEOIPH_H */