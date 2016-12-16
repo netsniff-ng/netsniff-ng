netsniff-ng/netsniff-ng

commit d6d511ecff241d459a70e270ac98d107faffdd45

trafgen: proto: Zero out newly allocated struct packet
When allocating a new struct packet, the headers and headers_count
fields are not zeroed and retain whatever value the allocator returned.
Incidentally, this usually seems to have been zero. But on some systems
(e.g. Ubuntu 16.04 with a self-compiled 4.9) it is not and we hit the
following bug_on:

  trafgen: trafgen_proto.c:135: proto_header_push: Assertion `!(pkt->headers_count >= 16)' failed.

Fix this by properly zeroing the entire struct packet.

Fixes: e7dd63060e44 ("trafgen: proto: Update field value at runtime")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

trafgen_parser.y
@@ -117,8 +117,7 @@ static inline void __init_new_packet_slot(struct packet *slot)
 
 static inline void __init_new_counter_slot(struct packet_dyn *slot)
 {
-	slot->cnt = NULL;
-	slot->clen = 0;
+	memset(slot, 0, sizeof(*slot);
 }
 
 static inline void __init_new_randomizer_slot(struct packet_dyn *slot)

commit 374141decf7c9978fd2c535099440d68e11e7683

trafgen: proto: Zero out the correct packet slot
In the third attempt, finally do the packet slot zeroing correctly. Zero
the struct packet in __init_new_packet_slot() not the struct packet_dyn
in __init_new_counter_slot().

Don't know what hit me yesterday...

Fixes: d6d511ecff24 ("trafgen: proto: Zero out newly allocated struct packet")
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

trafgen_parser.y
@@ -111,13 +111,13 @@ static inline int test_ignore(void)
 
 static inline void __init_new_packet_slot(struct packet *slot)
 {
-	slot->payload = NULL;
-	slot->len = 0;
+	memset(slot, 0, sizeof(*slot));
 }
 
 static inline void __init_new_counter_slot(struct packet_dyn *slot)
 {
-	memset(slot, 0, sizeof(*slot));
+	slot->cnt = NULL;
+	slot->clen = 0;
 }
 
 static inline void __init_new_randomizer_slot(struct packet_dyn *slot)