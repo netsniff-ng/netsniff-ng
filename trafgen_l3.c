netsniff-ng/netsniff-ng

commit c4e07d5142c8991d532804ff60b5cf16029c0497

trafgen: l3: Support interface without IP address
Move default source address setting to packet_finish so that we do not
need to get the device's address if the source address is set in the
packet. Without this, trafgen does not work with an interface without
address configured. In addition, in the case failing to get the address
for an interface, intead of panic, it now prints a warning and uses a
value of 0.0.0.0.

Signed-off-by: Ken Wu <abawwu@gmail.com>
Reviewed-by: Vadim Kochan <vadim4j@gmail.com>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

trafgen_l3.c
@@ -40,7 +40,6 @@ static void ipv4_header_init(struct proto_hdr *hdr)
 
 	proto_field_set_default_u8(hdr, IP4_VER, 4);
 	proto_field_set_default_u8(hdr, IP4_IHL, 5);
-	proto_field_set_default_dev_ipv4(hdr, IP4_SADDR);
 }
 
 static void ipv4_field_changed(struct proto_field *field)
@@ -81,6 +80,7 @@ static void ipv4_packet_finish(struct proto_hdr *hdr)
 
 	total_len = pkt->len - hdr->pkt_offset;
 	proto_field_set_default_be16(hdr, IP4_LEN, total_len);
+	proto_field_set_default_dev_ipv4(hdr, IP4_SADDR);
 
 	ipv4_csum_update(hdr);
 }
@@ -140,7 +140,6 @@ static void ipv6_header_init(struct proto_hdr *hdr)
 	proto_header_fields_add(hdr, ipv6_fields, array_size(ipv6_fields));
 
 	proto_field_set_default_be32(hdr, IP6_VER, 6);
-	proto_field_set_default_dev_ipv6(hdr, IP6_SADDR);
 }
 
 static void ipv6_field_changed(struct proto_field *field)
@@ -161,6 +160,7 @@ static void ipv6_packet_finish(struct proto_hdr *hdr)
 	uint16_t total_len = pkt->len - hdr->pkt_offset - IPV6_HDR_LEN;
 
 	proto_field_set_default_be16(hdr, IP6_LEN, total_len);
+	proto_field_set_default_dev_ipv6(hdr, IP6_SADDR);
 }
 
 static void ipv6_set_next_proto(struct proto_hdr *hdr, enum proto_id pid)

trafgen_proto.c
@@ -364,8 +364,10 @@ static void __proto_field_set_dev_ipv4(struct proto_hdr *hdr, uint32_t fid,
 		return;
 
 	ret = device_address(ctx.dev, AF_INET, &ss);
-	if (ret < 0)
-		panic("Could not get device IPv4 address\n");
+	if (ret < 0) {
+		fprintf(stderr, "Warning: Could not get device IPv4 address for %s\n", ctx.dev);
+		return;
+	}
 
 	ss4 = (struct sockaddr_in *) &ss;
 	__proto_field_set_bytes(hdr, fid, (uint8_t *)&ss4->sin_addr.s_addr, is_default, false);
@@ -392,8 +394,10 @@ static void __proto_field_set_dev_ipv6(struct proto_hdr *hdr, uint32_t fid,
 		return;
 
 	ret = device_address(ctx.dev, AF_INET6, &ss);
-	if (ret < 0)
-		panic("Could not get device IPv6 address\n");
+	if (ret < 0) {
+		fprintf(stderr, "Warning: Could not get device IPv6 address for %s\n", ctx.dev);
+		return;
+	}
 
 	ss6 = (struct sockaddr_in6 *) &ss;
 	__proto_field_set_bytes(hdr, fid, (uint8_t *)&ss6->sin6_addr.s6_addr, is_default, false);

commit 1e1383fea55fb35cec96a352da93c2a31d9e897c

trafgen: l3: Fix checksum for UDP/TCP protos
While fixing the issue with getting of IPv4 address from device,
the setting of default src IPv4/IPv6 addresses was moved from
hdr->header_init(...) callback to hdr->packet_finish(...), but
packet_finish(...) is called in the following order:

    udp_hdr->packet_finish() - UDP csum calculation over IPv4/6 pseudo header
    ip4_hdr->packet_finish() - setting default src IPv4 address from dev
    ...

So src IPv4/6 address will be set after UDP/TCP csum calculation which
is wrong, so fixed issue by moving it to the hdr->header_init(...) stage
as it was before the c4e07d5142c8.

Fixes: c4e07d5142c8 ("trafgen: l3: Support interface without IP address")
Signed-off-by: Vadim Kochan <vadim4j@gmail.com>
Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

trafgen_l3.c
@@ -40,6 +40,7 @@ static void ipv4_header_init(struct proto_hdr *hdr)
 
 	proto_hdr_field_set_default_u8(hdr, IP4_VER, 4);
 	proto_hdr_field_set_default_u8(hdr, IP4_IHL, 5);
+	proto_hdr_field_set_default_dev_ipv4(hdr, IP4_SADDR);
 }
 
 static void ipv4_field_changed(struct proto_field *field)
@@ -80,7 +81,6 @@ static void ipv4_packet_finish(struct proto_hdr *hdr)
 
 	total_len = pkt->len - hdr->pkt_offset;
 	proto_hdr_field_set_default_be16(hdr, IP4_LEN, total_len);
-	proto_hdr_field_set_default_dev_ipv4(hdr, IP4_SADDR);
 
 	ipv4_csum_update(hdr);
 }
@@ -140,6 +140,7 @@ static void ipv6_header_init(struct proto_hdr *hdr)
 	proto_header_fields_add(hdr, ipv6_fields, array_size(ipv6_fields));
 
 	proto_hdr_field_set_default_be32(hdr, IP6_VER, 6);
+	proto_hdr_field_set_default_dev_ipv6(hdr, IP6_SADDR);
 }
 
 static void ipv6_field_changed(struct proto_field *field)
@@ -160,7 +161,6 @@ static void ipv6_packet_finish(struct proto_hdr *hdr)
 	uint16_t total_len = pkt->len - hdr->pkt_offset - IPV6_HDR_LEN;
 
 	proto_hdr_field_set_default_be16(hdr, IP6_LEN, total_len);
-	proto_hdr_field_set_default_dev_ipv6(hdr, IP6_SADDR);
 }
 
 static void ipv6_set_next_proto(struct proto_hdr *hdr, enum proto_id pid)