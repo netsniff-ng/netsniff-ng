15826585ecfb89ef6877e80f12751f5d4223811b
Jia Zhouyang <jiazhouyang@nudt.edu.cn>
2018-03-08 11:38:45 +0800

    mausezahn: Add error handling for some API
    When the APIs fail, check their return value and return.
    
    Signed-off-by: Zhouyang Jia jiazhouyang09@gmail.com

--- staging/cli.c
+++ staging/cli.c
@@ -525,10 +525,12 @@ int cli(void)
    
    // Create a socket
    s = socket(AF_INET, SOCK_STREAM, 0);
-   if (s==-1) {
-	   perror("socket");
+   if (s == -1) {
+	   return -1;
    }
-   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+   if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) == -1) {
+	   return -1;
+   }
 
    // Should we bind the CLI session to a specific interface?
    // TODO: This does nothing !?
@@ -544,8 +546,8 @@ int cli(void)
    servaddr.sin_family = AF_INET;
    inet_aton(mz_listen_addr, &servaddr.sin_addr);
    servaddr.sin_port = htons(mz_port); 
-   if (bind(s, (struct sockaddr *)&servaddr, sizeof(servaddr))==-1) {
-	   perror("bind");
+   if (bind(s, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+	   return -1;
    }
 
    if (!quiet) {
@@ -553,8 +555,8 @@ int cli(void)
                }
 
    // Wait for a connection
-   if (listen(s, 50)==-1) {
-	   perror("listen");
+   if (listen(s, 50) == -1) {
+	   return -1;
    }
 
    while ((x = accept(s, NULL, 0)))
