netsniff-ng/netsniff-ng

commit 4e47fd021a6945aa626eaef4446c5b547d8c2a85

trafgen: parser: Check read access to file before invoking cpp
If a non-accessible (or non-existing) file is passed to trafgen and the
-p/--cpp option is used, the preprocessor will fail but trafgen will
continue running and producing follow-up errors messages:

	cpp: error: foo.conf: No such file or directory
	cpp: warning: ‘-x c’ after last input file has no effect
	cpp: fatal error: no input files
	compilation terminated.
	     0 packets to schedule
	Enabled kernel qdisc bypass
	     0 bytes in total
	Running! Hang up with ^C!

	Enabled kernel qdisc bypass
	TX,V2: 0.50 MiB, 256 Frames, each 2048 Byte allocated
	TX,V2: 0.50 MiB, 256 Frames, each 2048 Byte allocated

		   0 packets outgoing
		   0 bytes outgoing
		   0 sec, 0 usec on CPU0 (0 packets)
		   0 sec, 0 usec on CPU1 (0 packets)

To avoid this, check that the file is readable before passing it to the
preprocessor and error out if is not readable.

Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

trafgen_parser.y
@@ -12,11 +12,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <stdint.h>
-#include <errno.h>
 #include <stdbool.h>
+#include <errno.h>
 #include <libgen.h>
+#include <signal.h>
+#include <unistd.h>
 #include <net/if_arp.h>
 #include <netinet/in.h>
 #include <linux/icmp.h>
@@ -1036,6 +1037,11 @@ void compile_packets(char *file, bool verbose, unsigned int cpu,
 {
 	char tmp_file[128];
 	int ret = -1;
+
+	if (access(file, R_OK)) {
+		fprintf(stderr, "Cannot access %s: %s!\n", file, strerror(errno));
+		die();
+	}
 
 	memset(tmp_file, 0, sizeof(tmp_file));
 	our_cpu = cpu;

commit ba5f7aecf33d62670f20021b4bc3679e6c50e6fc

trafgen: fixed '--in -' to work again with STDIN
This has been broken by commit 4e47fd021a6945aa626eaef4446c5b547d8c2a85.

Signed-off-by: Jaroslav Škarvada <jskarvad@redhat.com>

trafgen_parser.y
@@ -1513,7 +1513,7 @@ void compile_packets(char *file, bool verbose, unsigned int cpu,
 	char tmp_file[128];
 	int ret = -1;
 
-	if (access(file, R_OK)) {
+	if (strncmp("-", file, strlen("-")) && access(file, R_OK)) {
 		fprintf(stderr, "Cannot access %s: %s!\n", file, strerror(errno));
 		die();
 	}